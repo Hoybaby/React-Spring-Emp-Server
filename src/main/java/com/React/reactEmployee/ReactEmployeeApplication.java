449700f6ea6186a5e1f15652b77d422bcd639c7c
Hoybaby <mbartek436@gmail.com>
2022-07-07 18:03:24 -0400

    after hiding the application folder, i added my connection so i can establish the connection. have not run the spring yet but will in a second.

--- src/main/java/com/React/reactEmployee/ReactEmployeeApplication.java
+++ src/main/java/com/React/reactEmployee/ReactEmployeeApplication.java
@@ -2,8 +2,10 @@ package com.React.reactEmployee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+
 public class ReactEmployeeApplication {
 
 	public static void main(String[] args) {
