18b6e5d40521108e9b5391beed9cbeab9175aea7
Hoybaby <mbartek436@gmail.com>
2022-07-07 17:50:17 -0400

    Initial commit

---
+++ src/main/java/com/React/reactEmployee/ReactEmployeeApplication.java
@@ -0,0 +1,13 @@
+package com.React.reactEmployee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactEmployeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/React/reactEmployee/ReactEmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.React.reactEmployee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactEmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
